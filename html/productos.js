d92edc9a7793804731eeb334feb303b86fdba577
adrianpitaluac <adrianpitaluac@gmail.com>
2023-01-05 20:42:28 -0600

    1

---
+++ html/productos.js
@@ -0,0 +1,10 @@
+const calcetas = [
+    {id: 1,producto: "Nike Cinturón GDE", img: "https://www.coppel.com/images/catalog/pr/1383542-1.jpg", marca: "Nike", precio: 25},
+    {id: 2,producto: "Restmi 8 Adidas Sucks", img: "https://http2.mlstatic.com/D_NQ_NP_640779-MLM40888210053_022020-O.jpg", marca: "Adidas", precio: 30},
+    {id: 3,producto: "Nike Blazers 77", img: "https://www.innvictus.com/medias/tenis-nike-blazer-mid-77-vintage-white-black-in-BQ6806-100-4.png?context=bWFzdGVyfGltYWdlc3wxOTIwMzd8aW1hZ2UvcG5nfGltYWdlcy9oNTQvaDZiLzEwMTQxNzIwNzcyNjM4LnBuZ3w2OWQ1YWM2MWIzOWVjODk2NjlkYTUyN2I2NWE5ODFlYTczZTM4NjQ4NGE5YTFiZTlkMWZmYTcyOTY1ZDBhZGU1", marca: "Nike", "precio": 35},
+    {id: 4,producto: "Dri-FIT Slim Fit ", img: "https://static.nike.com/a/images/t_default/f11b484a-96ce-4be1-84da-82d45d9589b3/pro-dri-fit-mens-slim-fit-long-sleeve-top-9rR6dS.png", marca: "Adidas", "precio": 40},
+    {id: 5,producto: "Workout Gloves Nike", img: "https://www.innovasport.com/medias/guantes-nike-gym-elemental-ch-is-N.LG.D2.010.SL-1.jpg?context=bWFzdGVyfGltYWdlc3wyNjAwMjF8aW1hZ2UvanBlZ3xpbWFnZXMvaDQ2L2hiOS85NzgzNTY0NDY4MjU0LmpwZ3wzOWQwNGI0ZGFhOGNlNDZjNzhmNjA1MTQ2MTNlZDdkNWJiZDI5Mjg5NWJkMzU5ZDU5Y2IzZGQyOTMwN2EwNmQ0", marca: "Adidas", precio: 30},
+    {id: 6,producto: "Short Workout Men", img: "https://media.kohlsimg.com/is/image/kohls/3450361_Game_Royal?wid=500&hei=500&op_sharpen=1", marca: "Nike", "precio": 35},
+    {id: 7,producto: "Nike Air Force One Mid", img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d9cad8f4-f7f7-441f-b94d-faa4e623abbf/calzado-air-force-1-high-KqX0hQ.png", marca: "Adidas", "precio": 40},
+    {id: 8,producto: "Restmi 8 Adidas Sucks", img: "https://http2.mlstatic.com/D_NQ_NP_640779-MLM40888210053_022020-O.jpg", marca: "Adidas", precio: 30}
+]
